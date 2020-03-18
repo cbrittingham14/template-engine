81c381c88bad5638fa93bc267294f1ec69a41f70
chizz <cbrittingham@gmail.com>
2020-03-18 16:39:40 -0500

    switched all working code from indexttest.js to app.js. fixed the whitespace error

--- Develop/app.js
+++ Develop/app.js
@@ -5,48 +5,99 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");​
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-const employeeArray = [];
-let questionCount = 0;
+let employeeArray = [];
 
-inquirer.prompt( {
-    type: 'number',
-    name: 'count',
-    message: 'How many employees do you have?'
-}).then(input => {
-    questionCount = input.count;
-});
+const questionArray = [
+    {
+        type: 'list',
+        name: 'type',
+        message: 'What type of employee is it?',
+        choices: ["manager", "engineer", "intern"]
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: 'what is the employees name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'what is the employees id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is the employees email'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'what is the office number',
+        when: (answers) => answers.type === "manager"
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'what school does he or she attend?',
+        when: (answers) => answers.type === "intern"
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'what is the github username?',
+        when: (answers) => answers.type === "engineer"
+    },
+    {
+        type: 'confirm',
+        name: 'add',
+        message: 'would you like to add another employee?'
+    },
+]
 
-inquirer.prompt( {
-    type: 'list',
-    name: 'type',
-    message: 'What type of employee is it?',
-    choices: ["manager", "engineer", "intern"]
-}).then(input => {
-    console.log(input.type);
-});
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+initPrompt(questionArray);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function initPrompt(questions){
+    inquirer.prompt(questions).then(input => {
+        switch(input.type){
+            case "manager":
+                employeeArray.push(new Manager(input.name, input.id, input.email, input.officeNumber));
+            break
+            case "engineer":
+                employeeArray.push(new Engineer(input.name, input.id, input.email, input.github));
+            break
+            case "intern":
+                employeeArray.push(new Intern(input.name, input.id, input.email, input.school));
+            break
+            default:
+                console.log("didnt get the question");
+        }
+        if(input.add){
+            initPrompt(questionArray);
+        }else{
+            makeHTML();
+        }
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-
-
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+function makeHTML(){
+    let html = render(employeeArray);
+    console.log("we ran makeHTML");
+    if (fs.existsSync(OUTPUT_DIR)){
+        console.log('found the directory');
+        fs.writeFile(outputPath, html, err=>{
+            if(err){
+                console.log(err);
+            }
+        })
+    } else{
+        console.log('should have made a file');
+        fs.mkdirSync(OUTPUT_DIR);
+        fs.writeFile(outputPath, html, err=>{
+            if(err){
+                console.log(err);
+            }
+        })
+    }
+}
